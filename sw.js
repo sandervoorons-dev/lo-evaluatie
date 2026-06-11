59eb9bf14b9f7c35a1eb5854796de699e02180ce
sandervoorons-dev <sander.voorons@hotmail.com>
2026-06-11 18:25:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = "lo-eval-v1";
+const ASSETS = [
+  "Evaluatie-app.html",
+  "manifest.webmanifest",
+  "icon-192.png",
+  "icon-512.png",
+  "apple-touch-icon.png"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const cp = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp));
+      return resp;
+    }).catch(() => caches.match("Evaluatie-app.html")))
+  );
+});
